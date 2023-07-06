5cd703f0db35783401c5a337ea57fcf73642299d
sandro <assandroa95@gmail.com>
2023-07-06 19:51:59 +0700

    running query

--- postgres/client.js
+++ postgres/client.js
@@ -1,17 +1,18 @@
-const { Client } = require('pg');
+const { Client } = require("pg");
 
 const pgclient = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: 'postgres',
-    password: 'postgres',
-    database: 'postgres'
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  user: "postgres",
+  password: "postgres",
+  database: "postgres",
 });
 
 pgclient.connect();
 
-pgclient.query('SELECT NOW()', (err, res) => {
-    if (err) throw err
-    console.log(res)
-    pgclient.end()
+pgclient.query("SELECT NOW()", (err, res) => {
+  console.log("running query");
+  if (err) throw err;
+  console.log(res);
+  pgclient.end();
 });
